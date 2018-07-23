f365a12410a1af4d44611c66dbcfe8078d91f805
Justin <justin4827@gmail.com>
2018-07-23 17:40:52 -0400

    Changed data receiving

--- OpenEyetap_Applications/Bluetooth/bluetoothserver.py
+++ OpenEyetap_Applications/Bluetooth/bluetoothserver.py
@@ -4,6 +4,17 @@ import bluetooth
 import json
 import threading
 from interface import interface
+import subprocess
+
+HEADER_LENGTH = 128
+
+def decode_file(data, filename):
+    f = open("temp/" + filename, "wb")
+    f.write(data)
+    f.close()
+
+cmd = 'sudo hciconfig hci0 piscan'
+subprocess.check_output(cmd, shell = True)
 
 gui = interface()
 gui.start()
@@ -25,32 +36,94 @@ print("Accepted connection from ",address)
 
 gui.clear_message()
 
-data = client_sock.recv(1024)
-print("Data received: ", str(data))
 
-while data:
+data = client_sock.recv(HEADER_LENGTH)
+print("Header received: ", str(data))
+print("Header size: ", len(data))
+
+while data:    
+    decoded_data = data.decode("utf-8")
+    
+    decoded_data = decoded_data[:decoded_data.find('}')]
+    decoded_data += '}'
+    
+    json_header = json.loads(decoded_data)  
+    
+    type = json_header["type"]
+    bytes = json_header["bytes"]
+    
+    current_bytes = 0;
+    data = b''
+    read_bytes = 0;
+    
+    while read_bytes < bytes:
+        print("Current file size: ", read_bytes)
+        bytes_to_read = min(1024, bytes - read_bytes)
+        data += client_sock.recv(bytes_to_read)
+        read_bytes = len(data)
+        print("Data received. Number of bytes read:", read_bytes, " / ", bytes)
+        
+        
+    if type == "file":
+        print("File size: ", len(data))
+        name = json_header["name"]
+        ext = json_header["ext"]
+          
+        filename = name + "." + ext
+        
+        decode_file(data, filename)
+    elif type == "notif":
+        print("Processing notification")
+        
+        json_string = data.decode("utf-8")
+        print(json_string)
+        notif_data = json.loads(json_string)
+        
+        print("Package: " + notif_data["package"])
+        print("Title: " + notif_data["title"])
+        print("Text: " + notif_data["text"])
+        print("Image: " + notif_data["img"])
+    
+        title = notif_data["title"]
+        package = notif_data["package"]
+        text = notif_data["text"]
+        image = "resources/notif.png"
+        if(notif_data["img"] != ""):
+            image = "temp/" + notif_data["img"]
+        print(image)
+    
+        gui.get_message(title, package, text, image)
+        
+    data = client_sock.recv(HEADER_LENGTH)
+    print("Header received: ", str(data))
+    
+    
+
+#while data:
     # client_sock.send('Echo => ' + str(data))
     # this is where data is processed
     
     # TODO: parse multiple json requests at once
-    print("Notification received.")
-    json_string = data.decode("utf-8")
+    #print("Notification received.")
+    #json_string = data.decode("utf-8")
 
-    notif_data = json.loads(json_string)
-    print("Package: " + notif_data["package"])
-    print("Title: " + notif_data["title"])
-    print("Text: " + notif_data["text"])
+    #notif_data = json.loads(json_string)
+    #print("Package: " + notif_data["package"])
+    #print("Title: " + notif_data["title"])
+    #print("Text: " + notif_data["text"])
     
-    title = notif_data["title"]
-    package = notif_data["package"]
-    text = notif_data["text"]
+    #title = notif_data["title"]
+    #package = notif_data["package"]
+    #text = notif_data["text"]
     
-    gui.get_message(title, package, text)
+    #gui.get_message(title, package, text)
 
     # receive next message
-    data = client_sock.recv(1024)
-    print("Data received:", str(data))
+    #data = client_sock.recv(1024)
+    #print("Data received:", str(data))
 
 client_sock.close()
 server_sock.close()
 gui.exit()
+
+
--- OpenEyetap_Applications/Bluetooth/interface.py
+++ OpenEyetap_Applications/Bluetooth/interface.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import PIL
+from PIL import ImageTk, Image
+import os
 import threading
 import time
 
@@ -17,17 +20,23 @@ class interface(threading.Thread):
 
         self.bottomFrame= Frame(self.root)
         self.bottomFrame.pack(side=BOTTOM)
+        self.bottomFrame.config(bg="black")
+        self.bottomFrame.pack()
 
         self.label_title = Label(self.topFrame, text="Waiting for connection...", fg="white", bg="black")
         self.label_title.config(font=("Arial", 40))
-
-        self.label_package = Label(self.topFrame, text="Ensure Bluetooth discovery is enabled for this device", fg="white", bg="black")
+        
+        self.image = ImageTk.PhotoImage(Image.open("resources/notif.png"))
+        self.image_panel = Label(self.topFrame, image=self.image, borderwidth=0, highlightthickness=0, background="black")
+        
+        self.label_package = Label(self.bottomFrame, text="Ensure Bluetooth discovery is enabled for this device", fg="white", bg="black")
         self.label_package.config(font=("Arial", 30))
         
-        self.label_text = Label(self.topFrame, text="Select this device in the companion Android app", fg="white", bg="black")
+        self.label_text = Label(self.bottomFrame, text="Select this device in the companion Android app", fg="white", bg="black")
         self.label_text.config(font=("Arial", 30))
         
-        self.label_title.pack()
+        self.label_title.pack(side=RIGHT)
+        self.image_panel.pack(side=LEFT, fill="both", expand="yes")
         self.label_package.pack()
         self.label_text.pack()
         
@@ -37,12 +46,19 @@ class interface(threading.Thread):
         self.label_title['text'] = ""
         self.label_package['text'] = ""
         self.label_text['text'] = ""
+        self.image = ImageTk.PhotoImage(Image.open("resources/black.jpg"))
+        self.image_panel.configure(image=self.image)
+        self.image_panel.image = self.image
+
     
-    def get_message(self, title, package, text):
+    def get_message(self, title, package, text, image):
+        print("Displaying notification")
         self.label_title['text'] = title
         self.label_package['text'] = package
         self.label_text['text'] = text
-        
+        self.image = ImageTk.PhotoImage(Image.open(image))
+        self.image_panel.configure(image=self.image)
+        self.image_panel.image = self.image
         time.sleep(2)
         
         self.clear_message()
